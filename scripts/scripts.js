14ba7a17ba7ef5cc5d782773f153802c69f98f31
Anton <i@anton-abbg.ru>
2023-06-24 16:29:39 +0700

    add page game

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,17 +1,9 @@
-// window.onload = function () {
-//   document.getElementById("check").onclick = function () {
-//     var nameInput = document.getElementById("login").value;
-//     if (nameInput == "" || nameInput.length == 0 || nameInput == null) {
-//       alert("Please enter your name");
-//     }
-//   };
-// };
-
 function check() {
   var nameInput = document.getElementById("login").value;
   if (nameInput == "" || nameInput.length == 0 || nameInput == null) {
     alert("Please enter your name");
   } else {
-    result.innerHTML = "Добро пожаловать " + nameInput;
+    result.innerHTML = "<strong>Добро пожаловать, " + nameInput + "!</strong>";
+    start.innerHTML = "<a class='startLink' href='pages/game.html'>Начать</a>";
   }
 }
